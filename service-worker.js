a1597d4458a7cc2325c0a89f5d2f134ae4eb92dc
gean.jesus <geansantos2010@hotmail.com>
2025-06-19 18:48:16 -0300

    (#Fix): Cleaning unecessaries references and configuring OneSignalSDK

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
